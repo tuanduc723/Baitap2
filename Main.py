b987cd65951557425792e479aabe31be8619f51b
tuanduc723 <duc.2274802010191@vanlanguni.vn>
2024-11-15 21:52:14 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,18 +1,16 @@
 import tkinter as tk
-from tkinter import ttk
 import psycopg2
 from GUI_Dangnhap import DangnhapGUI
 from GUI_Quanlysinhvien import QuanlysinhvienGUI
-from GUI_Quanlykhoa import QuanlykhoaGUI  # Import tab mới
 
 class MainApp:
     def __init__(self):
         self.db_connection = None
 
     def run(self):
-        # Đăng nhập
         self.root = tk.Tk()
         self.root.title("Đăng nhập")
+        
         login_gui = DangnhapGUI(self.root)
         self.root.mainloop()
 
@@ -24,20 +22,10 @@ class MainApp:
                 host="localhost",
                 port="5432"
             )
-            # Chuyển sang giao diện chính
             self.root = tk.Tk()
-            self.root.title("Hệ thống quản lý")
+            self.root.title("Quản lý sinh viên")
             
-            # Sử dụng Notebook cho các tab
-            notebook = ttk.Notebook(self.root)
-            notebook.pack(fill="both", expand=True)
-
-            # Tab quản lý sinh viên
-            QuanlysinhvienGUI(notebook, self.db_connection)
-
-            # Tab quản lý khoa và chuyên ngành
-            QuanlykhoaGUI(notebook, self.db_connection)
-
+            QuanlysinhvienGUI(self.root, self.db_connection)
             self.root.mainloop()
 
         if self.db_connection:
